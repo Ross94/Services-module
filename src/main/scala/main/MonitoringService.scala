5e7f04e30d01af2eb9bc25564c699dbb77f36471
UTENTE <daniele.rosetti@studio.unibo.it>
2018-05-08 12:28:21 +0200

    new sensor-hub version and improve jsonStream

--- src/main/scala/main/MonitoringService.scala
+++ src/main/scala/main/MonitoringService.scala
@@ -1,23 +1,22 @@
 package main
 
 import scala.io.Source
-
 import java.net.URI
 import java.util.concurrent.TimeUnit
 
-import api.events.SensorsHubEvents.DeviceCreated
+import api.events.SensorsHubEvents.{DeviceCreated, DeviceDeleted}
 import api.internal.DriversManager
 import api.sensors.DevicesManager
 import api.sensors.Sensors.Encodings
 import io.javalin.embeddedserver.jetty.websocket.WebSocketHandler
+import io.reactivex.subjects.PublishSubject
 import spi.service.{Service, ServiceMetadata}
 import org.json4s._
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
 import scala.concurrent.duration.Duration
-import rx.lang.scala
-import rx.lang.scala.Observable
+import rx.lang.scala.Subscription
 
 case class Procedure(sensor: String, rules: List[Rule])
 case class Rule(sign: String, threshold: Double, alarm: Int)
@@ -44,33 +43,35 @@ class MonitoringService extends Service {
     ret
   }
 
-  private[this] var jSonStream: scala.Observable[String] = _
+  private[this] var sensorStreams = Map[Int, Subscription]()
+  private[this] val jSonStream = PublishSubject.create[String]()
 
   DevicesManager.events.subscribe(_ match {
-      case deviceCreated: DeviceCreated =>
-        def sensorStream(): Observable[String] = deviceCreated.ds.dataStreams.head.observable.sample(Duration(800, TimeUnit.MILLISECONDS)).map(elem => {
+    case deviceCreated: DeviceCreated =>
+      deviceCreated.ds.dataStreams.foreach(stream => {
+        //adesso aggiungo l'id potrebbe non bastare se un sensore ha più sensori
+        sensorStreams += deviceCreated.ds.id -> stream.observable.sample(Duration(800, TimeUnit.MILLISECONDS)).map(elem => {
           var jsonElem = JObject()
-          jsonElem ~= ("name" -> deviceCreated.ds.name)
+          jsonElem ~= ("name" -> deviceCreated.ds.name) //potrebbe servire la pos e non solo il nome del sensore
           jsonElem ~= ("type" -> elem.parentDataStream.observedProperty.name)
           jsonElem ~= ("value" -> elem.result.toString.toDouble)
           jsonElem ~= ("timestamp" -> elem.resultTime.toString)
           jsonElem ~= ("alarm" -> valueValidator(elem.parentDataStream.observedProperty.name, elem.result))
           compact(render(jsonElem))
-        })
-        jSonStream == null match {
-          case true => jSonStream = sensorStream
-          case false => jSonStream = jSonStream merge sensorStream
-        }
-      case _ =>
+        }).subscribe(elem => jSonStream.onNext(elem))
+      })
+    case deviceDeleted: DeviceDeleted =>
+      sensorStreams(deviceDeleted.ds.id).unsubscribe()
+    case _ =>
   })
 
-  def getStream(): Option[Observable[String]] = if(jSonStream == null) Option.empty else Option(jSonStream.doOnSubscribe())
+  def getStream(): PublishSubject[String] = jSonStream
 
   override def init(metadata: ServiceMetadata): Unit = {}
 
   override def restart(): Unit = {}
 
-  override def dispose(): Unit = {}
+  override def dispose(): Unit = sensorStreams.foreach(stream => stream._2.unsubscribe())
 
 }
 
@@ -89,6 +90,8 @@ object MonitoringServiceTest extends App {
       DevicesManager.createDevice("t2", "", Encodings.PDF, new URI(""), drv)
   }
 
+  //t.getStream().subscribe(e => println(e))
+
   import io.javalin.Javalin
 
   val app = Javalin.start(8000)
@@ -97,13 +100,19 @@ object MonitoringServiceTest extends App {
     ws.onConnect(session => session.send("WELCOME!\n" + "Command list:\n" + commands))
     ws.onMessage((session, message) => {
       message match {
-        case "start" => if(t.getStream().nonEmpty)
-          t.getStream().get.subscribe(jsonElem => session.getRemote.sendString(jsonElem))
-          else session.getRemote.sendString("empty stream: no sensors found")
+        case "start" =>
+          t.getStream().subscribe(jsonElem => session.getRemote.sendString(jsonElem))
         case _ => session.getRemote.sendString("UNKNOWN COMMAND!\n" + commands)
       }
     })
   })
 
-  if(t.getStream().nonEmpty) t.getStream().get.subscribe(e => println(e)) else println("empty")
+  Thread.sleep(10000)
+
+  d1.foreach {
+    drv =>
+      DevicesManager.createDevice("t3", "", Encodings.PDF, new URI(""), drv)
+      DevicesManager.deleteDevice(0)
+      println("DELETED!")
+  }
 }
