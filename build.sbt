75f4c9f43174f508b2c768265cd50952537d6f78
Ross94 <daniele.rosetti@hotmail.it>
2018-05-15 19:14:23 +0200

    Cambia tutto

--- build.sbt
+++ build.sbt
@@ -13,7 +13,6 @@ libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-reflect" % "2.12.4",
   "org.scala-lang" % "scala-compiler" % "2.12.4",
   "org.json4s" % "json4s-jackson_2.12" % "3.6.0-M2",
-  "io.reactivex" %% "rxscala" % "0.26.5",
   "io.reactivex.rxjava2" % "rxjava" % "2.1.10",
   "com.github.pureconfig" %% "pureconfig" % "0.9.0",
   "org.scalamacros" % "paradise_2.12.4" % "2.1.1",
--- src/main/scala/main/MonitoringService.scala
+++ src/main/scala/main/MonitoringService.scala
@@ -2,21 +2,26 @@ package main
 
 import scala.io.Source
 import java.net.URI
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.Executors
 
+import api.config.Preferences
+import api.events.{EventBus, EventLogging}
 import api.events.SensorsHubEvents.{DeviceCreated, DeviceDeleted}
-import api.internal.DriversManager
+import api.internal.{DeviceController, DriversManager, TaskingSupport}
 import api.sensors.DevicesManager
 import api.sensors.Sensors.Encodings
+import fi.oph.myscalaschema.extraction.ObjectExtractor
 import io.javalin.embeddedserver.jetty.websocket.WebSocketHandler
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
+import org.apache.log4j.{Level, LogManager}
 import spi.service.{Service, ServiceMetadata}
 import org.json4s._
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
-import scala.concurrent.duration.Duration
-import rx.lang.scala.Subscription
+import scala.collection.concurrent.TrieMap
 
 case class Procedure(sensor: String, rules: List[Rule])
 case class Rule(sign: String, threshold: Double, alarm: Int)
@@ -25,6 +30,8 @@ class MonitoringService extends Service {
   implicit val _ = DefaultFormats
 
   private[this] val rules = (parse(Source.fromFile("assets/config/thresholds.json").mkString) \ "allRules").extract[List[Procedure]]
+
+  Preferences.configure("sh-prefs.conf")
 
   def valueValidator(sensorType: String, value: Any): Int = {
 
@@ -43,14 +50,15 @@ class MonitoringService extends Service {
     ret
   }
 
-  private[this] var sensorStreams = Map[Int, Subscription]()
+  private[this] val sensorStreams = TrieMap[Int, Disposable]()
   private[this] val jSonStream = PublishSubject.create[String]()
 
-  DevicesManager.events.subscribe(_ match {
+  EventBus.events.subscribe(_ match {
     case deviceCreated: DeviceCreated =>
       deviceCreated.ds.dataStreams.foreach(stream => {
         //adesso aggiungo l'id potrebbe non bastare se un sensore ha più sensori
-        sensorStreams += deviceCreated.ds.id -> stream.observable.sample(Duration(800, TimeUnit.MILLISECONDS)).map(elem => {
+        sensorStreams.put(deviceCreated.ds.id,
+          stream.observable.map[String](elem => {
           var jsonElem = JObject()
           jsonElem ~= ("name" -> deviceCreated.ds.name) //potrebbe servire la pos e non solo il nome del sensore
           jsonElem ~= ("type" -> elem.parentDataStream.observedProperty.name)
@@ -58,10 +66,10 @@ class MonitoringService extends Service {
           jsonElem ~= ("timestamp" -> elem.resultTime.toString)
           jsonElem ~= ("alarm" -> valueValidator(elem.parentDataStream.observedProperty.name, elem.result))
           compact(render(jsonElem))
-        }).subscribe(elem => jSonStream.onNext(elem))
+        }).subscribe(elem => jSonStream.onNext(elem)))
       })
     case deviceDeleted: DeviceDeleted =>
-      sensorStreams(deviceDeleted.ds.id).unsubscribe()
+      sensorStreams(deviceDeleted.ds.id).dispose()
     case _ =>
   })
 
@@ -71,42 +79,76 @@ class MonitoringService extends Service {
 
   override def restart(): Unit = {}
 
-  override def dispose(): Unit = sensorStreams.foreach(stream => stream._2.unsubscribe())
+  override def dispose(): Unit = sensorStreams.foreach(stream => stream._2.dispose())
 
+  override def start(): Unit = {}
 }
 
 object MonitoringServiceTest extends App {
+  //Remove jetty log
+  System.setProperty("org.eclipse.jetty.util.log.class", "org.eclipse.jetty.util.log.StdErrLog")
+  System.setProperty("org.eclipse.jetty.LEVEL", "OFF")
 
-  val t = new MonitoringService()
-  t.init(null)
+  import scala.collection.JavaConverters._
+  LogManager.getCurrentLoggers.asScala foreach {
+    case l: org.apache.log4j.Logger =>
+      if(!l.getName.startsWith("sh.")) l.setLevel(Level.OFF)
+  }
 
-  val d1 = DriversManager.instanceDriver("driver 1")
-  d1.foreach {
+  val service = new MonitoringService()
+  service.init(null)
+
+  ObjectExtractor.overrideClassLoader(DriversManager.cl)
+
+  val temperatureDriver = DriversManager.instanceDriver("driver 1")
+  temperatureDriver.foreach {
     drv =>
       drv.controller.init()
       drv.controller.start()
-      drv.config.configure("conf.conf")
-      DevicesManager.createDevice("t1", "", Encodings.PDF, new URI(""), drv)
-      DevicesManager.createDevice("t2", "", Encodings.PDF, new URI(""), drv)
+      drv.config.configure("temperature.conf")
+      DevicesManager.createDevice("temp", "", Encodings.PDF, new URI(""), drv)
   }
 
-  //t.getStream().subscribe(e => println(e))
+  val heartbeatDriver = DriversManager.instanceDriver("driver 2")
+  heartbeatDriver.foreach {
+    drv =>
+      drv.controller.init()
+      drv.controller.start()
+      drv.config.configure("heartbeat.conf")
+      DevicesManager.createDevice("heart", "", Encodings.PDF, new URI(""), drv)
+  }
+
+  val beepDriver = DriversManager.instanceDriver("driver 3")
+  beepDriver.foreach {
+    drv =>
+      drv.controller.init()
+      drv.controller.start()
+      DevicesManager.createDevice("bell", "", Encodings.PDF, new URI(""), drv)
+  }
+
+  //service.getStream().subscribe(e => println(e))
 
   import io.javalin.Javalin
 
-  val app = Javalin.start(8000)
+  val app = Javalin.start(args.headOption.getOrElse("8000").toInt)
   app.ws("/jsonStream", (ws: WebSocketHandler) => {
-    val commands = "start => get json stream\n"
-    ws.onConnect(session => session.send("WELCOME!\n" + "Command list:\n" + commands))
-    ws.onMessage((session, message) => {
+    ws.onConnect(session => service.getStream().observeOn(Schedulers.from(Executors.newSingleThreadExecutor()))
+        .subscribe(jsonElem => session.send(jsonElem)))
+    ws.onMessage((_, message) => {
       message match {
-        case "start" =>
-          t.getStream().subscribe(jsonElem => session.getRemote.sendString(jsonElem))
-        case _ => session.getRemote.sendString("UNKNOWN COMMAND!\n" + commands)
+        case _ =>
+          beepDriver.foreach {
+            drv =>
+              drv.controller match {
+                case ctrl: DeviceController with TaskingSupport =>
+                  ctrl.send("ring-bell", """{"duration":1000, "sleep":250}""").subscribe(e => println("maybe: " + e))
+              }
+          }
+          println("unknown response: " + message)
       }
     })
   })
-
+  /*
   Thread.sleep(10000)
 
   d1.foreach {
@@ -115,4 +157,9 @@ object MonitoringServiceTest extends App {
       DevicesManager.deleteDevice(0)
       println("DELETED!")
   }
+  */
+}
+
+object Malevole extends App {
+    java.awt.Toolkit.getDefaultToolkit.beep()
 }
